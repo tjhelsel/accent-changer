626103bec2f3f74451aad45846213b5ab661ce34
tjhelsel <terence.helsel@gmail.com>
2019-06-22 23:14:35 -0400

    feat: convertText function converts schwa, u, and i to stressed variants

--- utils/convertText.js
+++ utils/convertText.js
@@ -1,10 +1,52 @@
+const haveNoStress = {
+  a: true,
+  as: true,
+  at: true,
+  but: true,
+  of: true,
+  on: true,
+  per: true,
+  than: true,
+  to: true,
+  an: true,
+  the: true,
+  and: true
+};
+
+const stressedVowels = {
+  ə: 'ʌ',
+  i: 'iː',
+  u: 'uː'
+};
+
 //regex for detecting schwa, u, and i in stressed syllable:
-const haveNoStress = {a: true, as: true, at: true, but: true, of: true, on: true, per: true, than: true, to: true, an: true, the: true, and: true}
 
+const stressedSchwa = /[ˈˌ][^ˈˌiʌæɑeɪaʊɛrɔouə]*ə/;
 
-/[ˈˌ][^iʌæɑeɪaʊɛrɔou]*ə/
+const stressedU = /[ˈˌ][^ˈˌiʌæɑeɪaʊɛərɔou]*u/;
 
-/[ˈˌ][^iʌæɑeɪaʊɛərɔo]*u/
+const stressedI = /[ˈˌ][^ˈˌʌæɑeɪaʊɛərɔoui]*i/;
 
-/[ˈˌ][^ʌæɑeɪaʊɛərɔou]*i/
+//regex for detecting one-syllable words with schwa, u, or i
 
+// /[^ˈˌ]*ə[^ˈˌ]*/
+
+// /[^ˈˌ]*i[^ˈˌ]*/
+
+// /[^ˈˌ]*u[^ˈˌ]*/
+
+const convertText = (word, pron) => {
+  if (haveNoStress[word]) return pron;
+  let newPron = pron;
+  if (pron.includes('ˈ')) {
+    newPron = newPron.replace(stressedSchwa, '$1,ʌ');
+    newPron = newPron.replace(stressedI, '$1,iː');
+    newPron = newPron.replace(stressedU, '$1,uː');
+    return newPron;
+  } else {
+    newPron = newPron.replace('ə', 'ʌ');
+    newPron = newPron.replace('i', 'iː');
+    newPron = newPron.replace('u', 'uː');
+    return newPron;
+  }
+};
